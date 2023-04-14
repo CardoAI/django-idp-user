f917f1f25a3d95ea06368c21a56f454096a0b2a8
godd0t <lirrishala@gmail.com>
2023-04-14 12:11:09 +0200

    feat: add user async service && idp channels backend

--- idp_user/auth/authentication.py
+++ idp_user/auth/authentication.py
@@ -1,17 +1,19 @@
 import logging
 from typing import Callable, List, Literal, Optional
 
+import aiohttp
 import jwt
 import requests
 from django.conf import settings
 from django.contrib.auth.backends import ModelBackend
 from rest_framework import authentication, status
 from rest_framework.request import Request
+from utils.typing import JwtData
 
 from idp_user.models import User
-from idp_user.typing import JwtData
+from idp_user.services.async_user import UserServiceAsync
 from idp_user.utils.exceptions import AuthenticationError, MissingHeaderError
-from idp_user.utils.functions import get_or_none
+from idp_user.utils.functions import get_or_none, parse_query_params_from_scope
 
 logger = logging.getLogger(__name__)
 
@@ -116,3 +118,40 @@ class IDPAuthBackend(ModelBackend):
 
         if response.status_code == status.HTTP_200_OK:
             return response.json()["access"]
+
+
+class IDPChannelsAuthenticationMiddleware:
+    user_service_cls = UserServiceAsync
+
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        if scope.get("user"):
+            return await self.app(scope, receive, send)
+
+        query_params = parse_query_params_from_scope(scope)
+        authorization_access_token = query_params.get("authorization", [None])[0]
+        if not authorization_access_token:
+            return await self.app(scope, receive, send)
+        username = await self._get_username(authorization_access_token)
+        if not username:
+            return await self.app(scope, receive, send)
+        user = await self._get_user(username)
+        if user:
+            scope["user"] = user
+            scope["authorization_access_token"] = authorization_access_token
+
+        return await self.app(scope, receive, send)
+
+    async def _get_username(self, access_token):
+        headers = {"Authorization": f"Bearer {access_token}"}
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.get(f"{IDP_URL}/api/users/me/") as resp:
+                if resp.status != 200:
+                    return None
+                response = await resp.json()
+                return response.get("username")
+
+    async def _get_user(self, username):
+        return await self.user_service_cls.get_user(username=username)
