ad1d503d0bbd25cda2d76b2cc10250be966b945b
godd0t <lirrishala@gmail.com>
2023-04-22 00:58:56 +0200

    fix: async auth

--- idp_user/services/__init__.py
+++ idp_user/services/__init__.py
@@ -1,2 +1,6 @@
+import contextlib
+
+with contextlib.suppress(ImportError):
+    import rest_framework  # noqa
+    from idp_user.services.user import UserService  # noqa
 from idp_user.services.async_user import UserServiceAsync  # noqa
-from idp_user.services.user import UserService  # noqa
