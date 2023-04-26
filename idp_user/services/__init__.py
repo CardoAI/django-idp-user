756ff5ea179d62f6a66ff331522142973cbe01f5
godd0t <lirrishala@gmail.com>
2023-04-26 11:31:24 +0200

    Revert "fix: imports"
    This reverts commit 43a67f623c2f24c6e6710b133547009b8e0def39.

--- idp_user/services/__init__.py
+++ idp_user/services/__init__.py
@@ -1,2 +1,6 @@
-from idp_user.services.async_user import UserServiceAsync
-from idp_user.services.user import UserService
+from django.conf import settings
+
+if settings.IDP_USER_APP.get("ASYNC_MODE"):
+    from idp_user.services.async_user import UserServiceAsync
+else:
+    from idp_user.services.user import UserService
