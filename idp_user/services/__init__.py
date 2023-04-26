43a67f623c2f24c6e6710b133547009b8e0def39
godd0t <lirrishala@gmail.com>
2023-04-26 11:30:49 +0200

    fix: imports

--- idp_user/services/__init__.py
+++ idp_user/services/__init__.py
@@ -1,6 +1,2 @@
-from django.conf import settings
-
-if settings.IDP_USER_APP.get("ASYNC_MODE"):
-    from idp_user.services.async_user import UserServiceAsync
-else:
-    from idp_user.services.user import UserService
+from idp_user.services.async_user import UserServiceAsync
+from idp_user.services.user import UserService
