8a5330252dfb2183bb2cf0102220773453b28514
godd0t <lirrishala@gmail.com>
2023-05-23 01:32:09 +0200

    test

--- idp_user/apps.py
+++ idp_user/apps.py
@@ -2,7 +2,15 @@ from django.apps import AppConfig
 from django.conf import settings
 
 
+
+
+
 from django.db.models.signals import post_delete, post_save
+
+
+
+
+
 
 
 class IDPUserConfig(AppConfig):
