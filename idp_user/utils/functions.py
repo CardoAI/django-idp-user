c0c1d13dd22e0d3f40ece7e7e503ca446df9abce
Klajdi Çaushi <klajdi.caushi@cardoai.com>
2023-09-29 14:45:47 +0200

    style: small changes

--- idp_user/utils/functions.py
+++ idp_user/utils/functions.py
@@ -28,7 +28,8 @@ def update_record(record, save=True, **data):
 
 
 def cache_user_service_results(function):
-    from idp_user.settings import APP_IDENTIFIER
+    from idp_user.settings import APP_IDENTIFIER, IN_DEV
+
     def wrapper(user, *args, **kwargs):
         cache_key = f"{APP_IDENTIFIER}-{user.username}-{function.__name__}"
         for arg in args:
@@ -44,7 +45,6 @@ def cache_user_service_results(function):
             cache.set(cache_key, json.dumps(result))
             return result
 
-    from idp_user.settings import IN_DEV
     if IN_DEV or not settings.IDP_USER_APP.get('USE_REDIS_CACHE', False):
         return function
 
@@ -57,6 +57,7 @@ def get_kafka_bootstrap_servers(include_uri_scheme=True):
     We have to find the bootstrap servers and create the connection using them.
     """
     from idp_user.settings import AWS_S3_REGION_NAME
+
     if kafka_arn := settings.KAFKA_ARN:
         resource = boto3.client("kafka", region_name=AWS_S3_REGION_NAME)
         response = resource.get_bootstrap_brokers(
